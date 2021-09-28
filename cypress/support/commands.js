4235f0eb474b148ebe416923f6bece4bc5d8040a
decemberamit <37213286+decemberamit@users.noreply.github.com>
2021-09-28 17:30:41 +0100

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -35,3 +35,101 @@ Cypress.Commands.add('selectProduct', (productName) => {
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+
+/// <reference types="Cypress" />
+
+import {tokenCalls} from "../apiCalls/tokenCalls"
+import {textHelper} from "./helpers/textHelper"
+import {commonMethods} from "../integration/common/commonMethods";
+
+
+// -- Command to get the cloud token body and add to the fixtures
+Cypress.Commands.add('getCloudToken', () => { 
+  tokenCalls.getCloudToken()
+});
+
+// -- Command to get the Cpm Login token header and add to fixtures
+Cypress.Commands.add('getCpmLoginToken', () => {
+    tokenCalls.getCpmLoginToken()
+});
+
+// -- Command to select text
+Cypress.Commands.add("selection", { prevSubject: true }, (subject, fn) => {
+  textHelper.selection(subject, fn)
+});
+
+// -- Command to select text based on start and end string supplied
+Cypress.Commands.add("setSelection", { prevSubject: true }, (subject, query, endQuery) => {
+    textHelper.setSelection(subject, query, endQuery)
+});
+
+// -- Command to select text based on start and end string and then delete it
+Cypress.Commands.add("selectAndDelete", { prevSubject: true }, (subject, startString, endString) => {
+    textHelper.selectAndDelete(subject, startString, endString)    
+});
+
+// --- Command to select text based on start and end string and then insert text 
+Cypress.Commands.add("selectDeleteAndInsert", { prevSubject: true }, (subject, startString, endString) => {
+    textHelper.selectDeleteAndInsert(subject, startString, endString)
+});
+
+// -- Command to select text based on supplied string and rightclick on the text
+Cypress.Commands.add("selectAndRightclick", { prevSubject: true }, (subject, selectString) => {
+    textHelper.selectAndRightclick(subject, selectString)
+});
+
+Cypress.Commands.add("setCursor", { prevSubject: true }, (subject, query, atStart) => {
+    textHelper.setCursorBefore(subject, query, atStart)
+});
+
+// -- Command to set cursor at start of text of the element
+Cypress.Commands.add("setCursorBefore", { prevSubject: true }, (subject, query) => {
+    textHelper.setCursorBefore(subject, query)
+});
+
+// -- Command to set cursor at the end of text of the element
+Cypress.Commands.add("setCursorAfter", { prevSubject: true }, (subject, query) => {
+    textHelper.setCursorAfter(subject, query)
+});
+
+// -- Command to highlight text of element
+Cypress.Commands.add("highlightText", {prevSubject: true}, (subject) => {
+    textHelper.highlightText(subject)
+});
+
+// -- Command to get iframe
+Cypress.Commands.add('getIframe', (iframe) => {
+    commonMethods.getIframe(iframe)
+});
+
+// Command to Select Reinstate Line
+Cypress.Commands.add("SelectReinstateLine", {prevSubject: true}, (subject) => {
+    textHelper.SelectReinstateLine(subject)
+});
+
+// Command to Select Reinstate Selection
+Cypress.Commands.add("SelectReinstateSelection", {prevSubject: true}, (subject) => {
+    textHelper.SelectReinstateSelection(subject)
+});
+
+
+// Command to Select Reinstate Selection
+Cypress.Commands.add("SelectInsertLinkToDatafield", {prevSubject: true}, (subject) => {
+    textHelper.SelectInsertLinkToDatafield(subject)
+});
+
+
+// Command to Select Reinstate Selection
+Cypress.Commands.add("SelectRemoveLinkToDatafield", {prevSubject: true}, (subject) => {
+    textHelper.SelectRemoveLinkToDatafield(subject)
+});
+
+// -- Command to Insert Datafield
+Cypress.Commands.add('InsertDatafield', (iframe) => {
+    commonMethods.InsertDatafield(iframe)
+});
+
+
+
