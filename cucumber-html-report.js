d942db5b3674a3d51f18138344f9b9c0813ed5ec
decemberamit <37213286+decemberamit@users.noreply.github.com>
2021-09-28 17:34:36 +0100

    Update cucumber-html-report.js

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -25,3 +25,31 @@ report.generate({
         ]
     }
 });
+
+const report = require('multiple-cucumber-html-reporter');
+report.generate({
+    //for manual run change to
+    //jsonDir: 'cypress/cucumber-json/',
+	jsonDir: 'Maritech.CPM/Maritech.CPM.Ui.Cypress/cypress/cucumber-json/',
+	reportPath: './reports/cucumber-html-report.html',
+	metadata:{
+        browser: {
+            name: 'Chrome',
+            version: '91'
+        },
+        device: 'Agent machine',
+        platform: {
+            name: 'Windows 10 Enterprise',
+            version: '19043.1052'
+        }
+    },
+    customData: {
+        title: 'Run info',
+        data: [
+            {label: 'Project', value: 'Custom project'},
+            {label: 'Release', value: '1.2.3'},
+            {label: 'Cycle', value: 'B11221.34321'},
+            {label: 'Execution Start Time', value: ("0" + new Date().getDate()).slice(-2) + "-" + ("0" + (new Date().getMonth() + 1)).slice(-2) + "-" +  new Date().getFullYear() + " " + ("0" + new Date().getHours() + 1 ).slice(-2) + ":" + ("0" + new Date().getMinutes()).slice(-2) + ":" + ("0" + new Date().getSeconds()).slice(-2)}
+        ]
+    }
+});
